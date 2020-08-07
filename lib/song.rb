6a679c61bce52b95c1313ecf2ab69d10eed5b72f
boyloe <boyloe@gmail.com>
2020-08-07 11:44:03 -0600

    Write first for methods of Song class

--- lib/song.rb
+++ lib/song.rb
@@ -1,13 +1,54 @@
+require 'pry'
 class Song
   attr_accessor :name, :artist_name
   @@all = []
+
+  
 
   def self.all
     @@all
   end
 
-  def save
-    self.class.all << self
+  def save 
+    self.class.all << self 
+  end
+
+  def self.create 
+    song = Song.new
+    song.save
+    return song 
+  end
+
+  def self.new_by_name name
+     song = Song.new
+     song.name = name
+     song
+    #  binding.pry
+  end
+
+  def self.create_by_name name
+    song = Song.new
+    song.name = name
+    song.save
+    song
+  end
+
+  def self.find_by_name name
+    @@all.find do |song|
+      song.name == name
+    end
+  end
+
+  def find_or_create_by_name name
+    if self.find_by_name
+      return
+    else 
+      song = Song.new
+      song.name = name
+      song
+   
+      
+    end
   end
 
 end
